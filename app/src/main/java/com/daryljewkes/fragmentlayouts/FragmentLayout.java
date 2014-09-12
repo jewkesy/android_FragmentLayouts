1ff04e6d01bad774e91d21bd619c1d46e180738f
Daryl Jewkes <github@daryljewkes.com>
2014-09-12 09:54:24 +0100

    Initial commit

---
+++ app/src/main/java/com/daryljewkes/fragmentlayouts/FragmentLayout.java
@@ -0,0 +1,14 @@
+package com.daryljewkes.fragmentlayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FragmentLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_layout);
+    }
+
+}
---
+++ app/src/main/java/com/daryljewkes/fragmentlayouts/TitlesFragment.java
@@ -0,0 +1,78 @@
+package com.daryljewkes.fragmentlayouts;
+
+import android.app.FragmentTransaction;
+import android.app.ListFragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Daryl on 12/09/2014.
+ */
+public class TitlesFragment extends ListFragment {
+    boolean mDualPane;
+    int mCurCheckPosition = 0;
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("curChoice", mCurCheckPosition);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        showDetails(position);
+    }
+
+    private void showDetails(int index) {
+        mCurCheckPosition = index;
+
+        if (mDualPane) {
+            getListView().setItemChecked(index, true);
+
+            DetailsFragment details = (DetailsFragment) getFragmentManager().findFragmentById(R.id.details);
+
+            if (details == null || details.getShowIndex() != index) {
+                details = DetailsFragment.newInstance(index);
+
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+                ft.replace(R.id.details, details);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            } else {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), DetailsActivity.class);
+                intent.putExtra("index", index);
+
+                startActivity(intent);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayAdapter<String> connectArrayToListView = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1), SuperHeroInfo.NAMES);
+
+        setListAdapter(connectArrayToListView);
+
+        View detailsFrame = getActivity().findViewById(R.id.details);
+
+        mDualPane = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
+
+        if (savedInstanceState != null) {
+            mCurCheckPosition = savedInstanceState.getInt("curChoice");
+        }
+
+        if (mDualPane) {
+            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+            showDetails(mCurCheckPosition);
+        }
+
+    }
+}
